9cd98f1862ce4040053c4110d006780dc9210c1f
Nathan <nathan@gdquest.com>
2017-11-03 14:31:11 +0100

    refactor functions to return results and store as dictionary

--- _src/py/get_youtube_info.py
+++ _src/py/get_youtube_info.py
@@ -19,10 +19,14 @@ SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
+VIDEO_TITLE = 'video_titles'
+VIDEO_COUNT = 'count'
+
 def get_authenticated_service():
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
     credentials = flow.run_console()
     return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
 
 def get_channel_playlist(service, **kwargs):
     results = service.playlists().list(
@@ -30,40 +34,49 @@ def get_channel_playlist(service, **kwargs):
     ).execute()
 
     if results["pageInfo"]["totalResults"] > results["pageInfo"]["resultsPerPage"]:
-        numItems = results["pageInfo"]["resultsPerPage"]
+        results_count = results["pageInfo"]["resultsPerPage"]
     else:
-        numItems = results["pageInfo"]["totalResults"]
+        results_count = results["pageInfo"]["totalResults"]
 
-    for num in range(0, numItems):
-        print(results['items'][num]['snippet']['title'])
-        get_playlist_videos(service,
-            part='snippet',
-            playlistId=str(results['items'][num]["id"]),
-            maxResults=50
-        )
+    playlist_dict = {}
+    for index in range(0, results_count):
+        playlist_title = results['items'][index]['snippet']['title']
+        count, title = get_playlist_videos(service,
+                                           part='snippet',
+                                           playlistId=str(results['items'][index]["id"]),
+                                           maxResults=50)
+        playlist_dict[playlist_title] = {
+            VIDEO_COUNT: count,
+            VIDEO_TITLE: title
+        }
+    return playlist_dict
+
 
 def get_playlist_videos(service, **kwargs):
     results = service.playlistItems().list(
         **kwargs
     ).execute()
 
+    video_count = 0
     if results["pageInfo"]["totalResults"] > results["pageInfo"]["resultsPerPage"]:
-        numItems = results["pageInfo"]["resultsPerPage"]
+        video_count = results["pageInfo"]["resultsPerPage"]
     else:
-        numItems = results["pageInfo"]["totalResults"]
-    
-    print(numItems)
+        video_count = results["pageInfo"]["totalResults"]
 
-    for num in range(0, numItems):
-        print(" - ", results['items'][num]['snippet']['title'])
+    video_titles = []
+    for index in range(0, video_count):
+        title = results['items'][index]['snippet']['title']
+        video_titles.append(title)
+    return video_count, video_titles
+
 
 if __name__ == "__main__":
     # When running locally, disable OAuthlib's HTTPs verification. When
     # running in production *do not* leave this option enabled.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     youtube = get_authenticated_service()
-    get_channel_playlist(youtube,
-        part='snippet',
-        channelId='UCxboW7x0jZqFdvMdCFKTMsQ',
-        maxResults=50
-    )
+    data = get_channel_playlist(youtube,
+                                part='snippet',
+                                channelId='UCxboW7x0jZqFdvMdCFKTMsQ',
+                                maxResults=50)
+    print(data)
