72d8df8d1a701e558fa33400d3faa5ba3bc560b1
Guilherme Oliveira <htoguilherme@gmail.com>
2018-09-14 14:51:34 -0300

    Moved css build from gulp to hugo extended's integration
    Dependencies were removed from package.json and a new one was added (needed for postCSS)
    Autoprefixer configuration can now be found in postcss.config.js
    minify wasn't used since the docs state that using "outputStyle" "compressed" handles minification better
    
    Closes #55

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,6 @@
 const gulp = require('gulp')
       htmlmin = require('gulp-htmlmin')
-      cssmin = require('gulp-cssmin')
-      sass = require('gulp-sass')
       ghPages = require('gulp-gh-pages')
-      autoprefixer = require('gulp-autoprefixer')
-
-const scssWatchFiles = ['./_src/scss/**/*.scss']
-const scssSrcFile = './_src/scss/gdquest.scss'
-const cssOutputFolder = './static/css/'
-
-
-gulp.task('watch', function () {
-    gulp.watch(scssWatchFiles, ['scss']);
-});
-
-
-gulp.task('scss', function () {
-  return gulp.src(scssSrcFile)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false,
-            flexbox: 'no-2009'
-        }))
-    .pipe(cssmin())
-    .pipe(gulp.dest(cssOutputFolder))
-})
-
 
 gulp.task('htmlmin', function () {
   return gulp.src('./public/**/*.html')
@@ -35,7 +9,6 @@ gulp.task('htmlmin', function () {
     }))
     .pipe(gulp.dest('public'))
 })
-
 
 gulp.task('deploy', function () {
     return gulp.src('./public/**/*')
