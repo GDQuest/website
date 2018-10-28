0692df66de00043d8906f3804faa9b04bb535501
Nathan Lovato <nathan@gdquest.com>
2018-10-28 16:10:22 +0900

    Clean up code style in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const gulp = require('gulp')
-      htmlmin = require('gulp-htmlmin')
-      ghPages = require('gulp-gh-pages')
+const htmlmin = require('gulp-htmlmin')
+const ghPages = require('gulp-gh-pages')
 
-gulp.task('htmlmin', function () {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(gulp.dest('public'))
+gulp.task('htmlmin', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('public'))
 })
 
-gulp.task('deploy', function () {
+gulp.task('deploy', function() {
     return gulp.src('./public/**/*')
         .pipe(ghPages())
 })
