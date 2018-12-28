1ba0853883d43e3b6062b0da0f3d0e9486ab983a
Nathan Lovato <nathan@gdquest.com>
2018-12-29 06:47:38 +0900

    Remove gulp-htmlmin: use `hugo --minify` instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,5 @@
 const gulp = require('gulp')
-const htmlmin = require('gulp-htmlmin')
 const ghPages = require('gulp-gh-pages')
-
-gulp.task('htmlmin', function() {
-    return gulp.src('./public/**/*.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true
-        }))
-        .pipe(gulp.dest('public'))
-})
 
 gulp.task('deploy', function() {
     return gulp.src('./public/**/*')
