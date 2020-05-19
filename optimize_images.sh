62e411281d5d160d5cc715a885b9f9dd93905b6a
Nathan Lovato <nathan@gdquest.com>
2020-05-19 09:57:56 -0600

    Remove optimize_images.sh

--- optimize_images.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-for file in "$@"; do
-	echo "$file" | grep -E ".jpe?g$" || continue
-	convert "$file" -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB -gaussian-blur 0.05 image_converted.jpg
-done
