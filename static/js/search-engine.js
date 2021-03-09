c3af3a95bce8dfce2a7a412aef31d72c940bbb4e
Nathan Lovato <nathan@gdquest.com>
2021-03-09 08:19:36 -0600

    Format js code, tweak search behavior, update search styles

--- static/js/search-engine.js
+++ static/js/search-engine.js
@@ -6,12 +6,13 @@
  * @author Levrault
  */
 class SearchEngine {
-  searchInput = null
-  resultsField = null
-  resultsTemplate = null
-  fuse = null
-  searchQuery = ''
-  summaryInclude = 60
+  searchInput = null;
+  resultsField = null;
+  resultsTemplate = null;
+  fuse = null;
+  searchQuery = "";
+  summaryInclude = 60;
+  minQueryLength = 3;
   fuseOptions = {
     shouldSort: true,
     includeMatches: true,
@@ -22,13 +23,13 @@ class SearchEngine {
     maxPatternLength: 32,
     minMatchCharLength: 2,
     keys: [
-      { name: 'title', weight: 0.8 },
-      { name: 'tags', weight: 0.3 },
-      { name: 'description', weight: 0.3 },
-      { name: 'content', weight: 0.5 },
-      { name: 'uri', weight: 0.2 },
+      {name: "title", weight: 0.8},
+      {name: "tags", weight: 0.3},
+      {name: "description", weight: 0.3},
+      {name: "content", weight: 0.5},
+      {name: "uri", weight: 0.2},
     ],
-  }
+  };
 
   /**
    * Decode uri to get the search's query string
@@ -37,8 +38,8 @@ class SearchEngine {
    */
   static param(name) {
     return decodeURIComponent(
-      (location.search.split(name + '=')[1] || '').split('&')[0]
-    ).replace(/\+/g, ' ')
+      (location.search.split(name + "=")[1] || "").split("&")[0]
+    ).replace(/\+/g, " ");
   }
 
   /**
@@ -57,128 +58,134 @@ class SearchEngine {
     fuseOptions,
     summaryInclude,
   }) {
-    this.searchInput = searchInput
+    this.searchInput = searchInput;
 
     // live search
     this.searchInput.onkeyup = (e) => {
-      if (this.fuse === null) return
-      this.searchQuery = e.target.value
-      this.executeSearch(e.target.value)
+      if (this.fuse === null) return;
+      this.searchQuery = e.target.value;
 
-      var queryParams = new URLSearchParams(window.location.search)
-      queryParams.set('s', e.target.value)
-      history.replaceState(null, null, '?' + queryParams.toString())
-    }
+      var queryParams = new URLSearchParams(window.location.search);
+      queryParams.set("s", e.target.value);
+      history.replaceState(null, null, "?" + queryParams.toString());
 
-    this.resultsField = resultsField
-    this.resultsTemplate = resultsTemplate
-    this.url = url
+      if (this.searchQuery.length >= this.minQueryLength) {
+        this.executeSearch(e.target.value);
+      }
+    };
+
+    this.resultsField = resultsField;
+    this.resultsTemplate = resultsTemplate;
+    this.url = url;
     if (fuseOptions) {
-      this.fuseOptions = fuseOptions
+      this.fuseOptions = fuseOptions;
     }
 
     if (summaryInclude) {
-      this.summaryInclude = summaryInclude
+      this.summaryInclude = summaryInclude;
     }
 
-    this.searchQuery = this.constructor.param('s')
-    this.searchInput.value = this.searchQuery
+    this.searchQuery = this.constructor.param("s");
+    this.searchInput.value = this.searchQuery;
 
-    this.fetchArticles()
+    this.fetchArticles();
   }
 
   /**
    * Cache search index file
    */
   fetchArticles = () => {
-    const Http = new XMLHttpRequest()
-    Http.open('GET', this.url)
-    Http.send()
+    const Http = new XMLHttpRequest();
+    Http.open("GET", this.url);
+    Http.send();
 
     Http.onreadystatechange = (e) => {
-      this.fuse = new Fuse(JSON.parse(Http.response), this.fuseOptions)
+      this.fuse = new Fuse(JSON.parse(Http.response), this.fuseOptions);
       if (this.searchQuery) {
-        this.executeSearch(this.searchQuery)
+        this.executeSearch(this.searchQuery);
       }
-    }
-  }
+    };
+  };
 
   /**
    * Make a XMLHttpRequest to the hugo's generated json file (url props in constructor)
    * @param {*} searchQuery
    */
   executeSearch = (searchQuery) => {
-    var result = this.fuse.search(searchQuery)
-    if (result.length > 0) {
-      this.resultsField.innerHTML = ''
-      this.populateResults(result)
+    this.resultsField.innerHTML = "No results found.";
+    if (this.searchQuery.length >= this.minQueryLength) {
+      var result = this.fuse.search(searchQuery);
+      if (result.length > 0) {
+        this.populateResults(result);
+      }
     } else {
-      this.resultsField.innerHTML = 'No matches found'
+      this.resultsField.innerHTML = `Please type at least ${this.minQueryLength} characters.`;
     }
-  }
+  };
 
   populateResults = (result) => {
+    this.resultsField.innerHTML = "";
     result.forEach((value, key) => {
-      let contents = value.item.content
-      let snippet = ''
-      let snippetHighlights = []
+      let contents = value.item.content;
+      let snippet = "";
+      let snippetHighlights = [];
 
       if (this.fuseOptions.tokenize) {
-        snippetHighlights.push(this.searchQuery)
+        snippetHighlights.push(this.searchQuery);
       } else {
         value.matches.forEach((matchesValue) => {
-          if (matchesValue.key == 'tags') {
-            snippetHighlights.push(matchesValue.value)
-            return
+          if (matchesValue.key == "tags") {
+            snippetHighlights.push(matchesValue.value);
+            return;
           }
 
-          if (matchesValue.key != 'content') {
-            return
+          if (matchesValue.key != "content") {
+            return;
           }
 
           let start =
             matchesValue.indices[0][0] - this.summaryInclude > 0
               ? matchesValue.indices[0][0] - this.summaryInclude
-              : 0
+              : 0;
           let end =
             matchesValue.indices[0][1] + this.summaryInclude < contents.length
               ? matchesValue.indices[0][1] + this.summaryInclude
-              : contents.length
-          snippet += contents.substring(start, end)
+              : contents.length;
+          snippet += contents.substring(start, end);
           snippetHighlights.push(
             matchesValue.value.substring(
               matchesValue.indices[0][0],
               matchesValue.indices[0][1] - matchesValue.indices[0][0] + 1
             )
-          )
-        })
+          );
+        });
       }
 
       if (snippet.length < 1) {
-        snippet += contents.substring(0, this.summaryInclude * 2)
+        snippet += contents.substring(0, this.summaryInclude * 2);
       }
 
       // pull template from hugo template definition
-      let templateDefinition = this.resultsTemplate.innerHTML
+      let templateDefinition = this.resultsTemplate.innerHTML;
       // replace values
       let output = this.render(templateDefinition, {
         key: key,
         title: value.item.title,
         link: value.item.uri,
         snippet: snippet,
-      })
-      this.resultsField.innerHTML += output
+      });
+      this.resultsField.innerHTML += output;
 
       snippetHighlights.forEach((keyword) => {
-        let markInstance = new Mark(document.querySelector(`#summary-${key}`))
+        let markInstance = new Mark(document.querySelector(`#summary-${key}`));
         markInstance.unmark({
           done: function () {
-            markInstance.mark(keyword, {})
+            markInstance.mark(keyword, {});
           },
-        })
-      })
-    })
-  }
+        });
+      });
+    });
+  };
 
   /**
    * Use search-result-template to format result display
@@ -187,9 +194,9 @@ class SearchEngine {
    * @returns {string}
    */
   render = (templateString, data) => {
-    let conditionalMatches = null
-    let conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g
-    let copy = templateString
+    let conditionalMatches = null;
+    let conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
+    let copy = templateString;
 
     // since loop below depends on re.lastIndex, we use a copy to capture any manipulations whilst inside the loop
     while (
@@ -197,20 +204,20 @@ class SearchEngine {
     ) {
       if (data[conditionalMatches[1]]) {
         //valid key, remove conditionals, leave contents.
-        copy = copy.replace(conditionalMatches[0], conditionalMatches[2])
+        copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
       } else {
         //not valid, remove entire section
-        copy = copy.replace(conditionalMatches[0], '')
+        copy = copy.replace(conditionalMatches[0], "");
       }
     }
-    templateString = copy
+    templateString = copy;
     //now any conditionals removed we can do simple substitution
-    var key, find, re
+    var key, find, re;
     for (key in data) {
-      find = '\\$\\{\\s*' + key + '\\s*\\}'
-      re = new RegExp(find, 'g')
-      templateString = templateString.replace(re, data[key])
+      find = "\\$\\{\\s*" + key + "\\s*\\}";
+      re = new RegExp(find, "g");
+      templateString = templateString.replace(re, data[key]);
     }
-    return templateString
-  }
+    return templateString;
+  };
 }
