414eaa80b457978616b769127ca20d725688ff60
Nathan Lovato <nathan@gdquest.com>
2021-10-14 08:09:05 -0600

    fix: factor in time offset to calculate times in UTC

--- static/js/kickstarter-countdown.js
+++ static/js/kickstarter-countdown.js
@@ -4,16 +4,24 @@ function kickstarterCountdown(dateString) {
   const hour = 60 * 60 * 1000,
     day = hour * 24;
 
-  const startTime = new Date("2021-10-15T12:00").getTime();
-  const endTime = new Date(dateString).getTime();
-  const now = new Date().getTime();
+  const startDate = new Date("2021-10-15T12:00");
+  const startTime = startDate.getTime();
+  const timeOffsetUTC = startDate.getTimezoneOffset() * 60 * 1000;
+  const startTimeUTC = startTime - timeOffsetUTC;
+
+  // November 1st
+  const endTime = Date.UTC(2021, 10, 1);
+
   const updateCountdown = () => {
-    const distance = endTime - now;
+    const now = new Date().getTime();
+    const distance = endTime - now + timeOffsetUTC;
     const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    console.log(days, hours);
 
     const countdown = document.getElementById("kickstarter-countdown");
     if (now < startTime) {
-      const hours = Math.floor((startTime - now) / hour);
+      const hours = Math.floor((startTimeUTC - now) / hour);
       countdown.innerHTML = `Starting in ${hours} hours`;
     } else if (days >= 3) {
       countdown.innerText = `${days} days left`;
