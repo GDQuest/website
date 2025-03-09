77f8293b063bb3d6887d3a004cfb2d274ac1a0ac
xananax <me@xananax.com>
2025-03-10 00:19:30 +0100

    chore: support for dark theme

---
+++ static/library/head.js
@@ -0,0 +1,1 @@
+const theme=localStorage.getItem("theme");if(theme==="dark"||!theme&&window.matchMedia("(prefers-color-scheme: dark)").matches){document.documentElement.classList.add("dark");}else{document.documentElement.classList.remove("dark");}
